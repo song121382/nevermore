398bac4cf02eada4d7de2ae48ef1a7b327747476
宋祥富 <songxiangfu@forenms.com>
2019-04-15 08:07:28 +0800

    mapper extend

---
+++ src/main/java/com/defence/nevermore/mapper/TestMapperExtend.java
@@ -0,0 +1,24 @@
+package com.defence.nevermore.mapper;
+
+import com.defence.nevermore.domain.Test;
+
+import java.util.List;
+
+/**
+ * @ProjectName: nevermore
+ * @Package: com.defence.nevermore.mapper
+ * @ClassName: TestMapperExtend
+ * @Description: java类作用描述
+ * @Author: songxiangfu
+ * @CreateDate: 2019/4/13 14:36
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2019/4/13 14:36
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ *
+ * mapper 拓展
+ *
+ */
+public interface TestMapperExtend {
+    List<Test> getTT();
+}
--- src/test/java/com/defence/nevermore/NevermoreApplicationTests.java
+++ src/test/java/com/defence/nevermore/NevermoreApplicationTests.java
@@ -1,22 +1,27 @@
 package com.defence.nevermore;
 
-import com.defence.nevermore.config.security.JwtConfig;
+import com.defence.nevermore.mapper.TestMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = NevermoreApplication.class)
 public class NevermoreApplicationTests {
 
+//    @Autowired
+//    private JwtConfig jwtConfig;
     @Autowired
-    private JwtConfig jwtConfig;
+    private TestMapper testMapper;
 
     @Test
     public void contextLoads() {
-        System.out.println(jwtConfig.getHeader());
+//        System.out.println(jwtConfig.getHeader());
+        List<com.defence.nevermore.domain.Test> list = testMapper.getTT();
 
     }
 
